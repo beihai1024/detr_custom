c80e5215b5c5567580cdb5be44ed8438b565935b
ningzhenyi <1275713985@qq.com>
2023-07-14 17:27:45 +0800

    修改路径

--- change.py
+++ change.py
@@ -1,6 +1,6 @@
 import torch
 
-pretrained_weights = torch.load("./coco_pretrain/detr-r50-e632da11.pth")
+pretrained_weights = torch.load("/content/coco_pretrain/detr-r50-e632da11.pth")
 
 num_class = 3 + 1
 pretrained_weights["model"]["class_embed.weight"].resize_(num_class + 1, 256)
